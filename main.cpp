d2b75322b0da53b46921221a2bee16fa704ae12c
Юлія <yuliia.parkhom@gmail.com>
2026-02-16 22:36:24 +0200

    branch1 commit 1

--- main.cpp
+++ main.cpp
@@ -4,5 +4,6 @@ int main(){
     std::cout<<"Hello,Git!"<<std::endl;
     std::cout<<"New line"<<std::endl;
     std::cout<<"You've added new file"<<std::endl;
+    std::cout<<"Created new branch, current is branch1"<<std::endl;
     return 0;
 }
